6227020c7dfe947aeda05d06a22dac541f1fd150
Mohin <mdmohin142@gmail.com>
2025-11-17 10:01:57 +0600

    complete the class-weekend-boring project

--- class12-materials/class-weekend-boring/js/main.js
+++ class12-materials/class-weekend-boring/js/main.js
@@ -1,10 +1,12 @@
-document.querySelector('#check').addEventListener('click', check)
-
-function check() {
-
-  const day = document.querySelector('#day').value
-
-  //Conditionals go here
-
-
-}
+const check = document.querySelector('#check')
+const displayTheOutput = document.querySelector('#placeToSee')
+check.addEventListener('click', ()=> {
+  const day = document.querySelector('#day').value.toLowerCase()
+  if(day === 'tuesday' || day === 'thursday'){
+    displayTheOutput.innerText = "class day"
+  }else if(day === 'saturday' || day === 'sunday'){
+    displayTheOutput.innerText = 'BORING DAY'
+  }else{
+    displayTheOutput.innerText = "WEEKEND"
+  }
+})
