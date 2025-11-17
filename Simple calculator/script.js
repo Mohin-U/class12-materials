cb618c1fca7d6b0f84771dc9a2d81f704aca749a
Mohin <mdmohin142@gmail.com>
2025-11-17 09:21:42 +0600

    modify the simple calculator project by my own way

--- Simple calculator/script.js
+++ Simple calculator/script.js
@@ -1,26 +1,24 @@
 let total = 0
 
 document.querySelector('#zero').addEventListener('click', makeZero)
-document.querySelector('#plusThree').addEventListener('click', makeThree)
-document.querySelector('#plusNine').addEventListener('click', makeNine)
-document.querySelector('#negativeTwo').addEventListener('click', makeNegativeTwo)
-
-function makeZero() {
-    total = 0
-    document.querySelector('#placeToPutResult').innerText = total
-}
-
-function makeThree() {
+document.querySelector('#plusThree').addEventListener('click', ()=>{
     total = total + 3
     document.querySelector('#placeToPutResult').innerText = total
-}
+})
 
-function makeNine() {
+document.querySelector('#plusNine').addEventListener('click', ()=>{
     total = total + 9
     document.querySelector('#placeToPutResult').innerText = total
-}
+})
 
-function makeNegativeTwo() {
+const negativeTwo = document.querySelector('#negativeTwo')
+negativeTwo.addEventListener('click', ()=> {
     total = total - 2
+    document.querySelector('#placeToPutResult').innerText = total
+})
+
+
+function makeZero(){
+    total = 0
     document.querySelector('#placeToPutResult').innerText = total
 }
